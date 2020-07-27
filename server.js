f0fb44a769fda3bf2615f81ade893f7b2d5afe9d
student <ecok@deakin.edu.au>
2020-07-27 23:26:55 +1000

    i commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express =require('express')
 var app =express()
 
+
 app.use(express.static(__dirname + '/public'));
 var adder=function(num1,num2){
     var result = num1+num2;
@@ -16,11 +17,32 @@ res.send('you got me ' +username);
 }
 
 )
+app.get('/name',function(req,res)
+
+
+{
+    let accounts= [
+
+{id:1,name:'alex',deposit:5},
+{id:2,name:'sarah',deposit:5},
+{id:3,name:'jim',deposit:15}
+    ]
+return res.send( Object.values (accounts));
+app.get('/name/:nameid',function(req,res){
+    return res.send(accounts [req.params.nameid]);
+    });
+
+}
+
+)
+
+
 app.get('/adder',function(req,res){
     var num1 =parseInt(req.query.num1);
     var num2=parseInt(req.query.num2);
     var result=adder(num1,num2)
     res.send('the result is '+result)
+
 })
 var port=3000
 app.listen(port)
